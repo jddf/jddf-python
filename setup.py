b0a0b33375ed7066259f025c2eec0167782ea1d8
Ulysse Carion <ulysse@segment.com>
2019-11-10 19:40:59 -0800

    Release 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jddf",
-    version="0.1.1",
+    version="0.1.2",
     author="Ulysse Carion",
     author_email="ulysse@segment.com",
     description="JSON Data Definition Format support for Python",
